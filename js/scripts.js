766b68f01f61d6d48c808e095bc1e65171c8bce8
Student <example@example.com>
2019-02-01 10:59:07 -0800

    add logic for both user and back

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,68 @@
 //back end logic
 
-function.pizza() {
-  this.size = []
-  this.topping = []
-  this.price=0
+function Pizza() {
+  this.size = [];
+  this.toppings = [];
+  this.price=0;
 }
 
-Pizza.prototype.userSize = function (size) {
-  if (size === "Yours-10inch"){
-  this.size = 10;
+  Pizza.prototype.userSize = function (size) {
+  if (size === "Yours") {
+    this.size = 10;
 
-  }else if(size === "Small-15inch"){
+  }else if(size === "Small") {
     this.size = 20;
 
-  }else if(size === "Medium-20inch"){
+  }else if(size === "Medium") {
     this.size = 30;
 
-  }else if(size === "Large-25inch"){
+  }else if(size === "Large") {
     this.size = 40;
-
-};
-
-Pizza.prototype.userTopping = function (topping) {
-  if (topping ==="olives") {
-    this.topping = 3;
-
-  }  (topping ==="mushrooms"){
-    this.topping = 5;
-
-  }  (topping ==="truffles"){
-    this.topping = 20;
-
-  }  (topping ==="gold"){
-    this.topping = 30;
   }
 
-};
+}
+
+Pizza.prototype.userToppings = function () {
+  if (this.toppings ==="olives") {
+    this.topping = 3;
+
+  }else if (this.toppings ==="mushrooms") {
+    this.topping = 5;
+
+  }else if (this.toppings ==="truffles"){
+    this.topping = 20;
+
+  }else if (this.toppings ==="gold"){
+    this.topping = 30;
+  }else {
+    this.toppings = 0;
+  }
+
+}
+
+
+
+Pizza.prototype.totalPrice = function() {
+  return this.size += this.toppings;
+}
+
+
+//front end logic
+$(document).ready(function() {
+
+  $("#form").submit(function(event) {
+    event.preventDefault();
+
+    var pizzaSize = parseInt($("#pizza-size").val());
+    var totalToppings = 0;
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      totalToppings += parseInt($(this).val());
+    });
+
+    var newPizzaOrder = new Pizza();
+
+    totalPrice=(pizzaSize + totalToppings);
+    $("#cost").text(totalPrice);
+    $(".result").show();
+  });
+});
